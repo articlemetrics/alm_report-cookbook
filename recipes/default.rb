c825dcdf158389b2d5eca2a89ec539e44453c16c
Martin Fenner <mfenner@plos.org>
2014-08-12 15:15:39 +0200

    adapted cookbook to changes in alm cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,45 @@
+# Add PPA for Ruby 2.x
+apt_repository 'brightbox-ruby-ng' do
+  uri          'http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu'
+  distribution 'precise'
+  components   ['main']
+  keyserver    'keyserver.ubuntu.com'
+  key          'C3173AA6'
+end
+
+# Upgrade openssl to latest version
+package "openssl" do
+  action :upgrade
+end
+
 # Install required packages
-%w{ruby1.9.3 curl}.each do |pkg|
+%w{libxml2-dev libxslt-dev ruby2.1 ruby2.1-dev curl}.each do |pkg|
   package pkg do
     action :install
   end
 end
+
 gem_package "bundler" do
   gem_binary "/usr/bin/gem"
 end
 
+file '/etc/hostname' do
+  content "#{node[:alm_report][:hostname]}\n"
+end
+
+file '/etc/hosts' do
+  content "127.0.0.1  localhost #{node[:alm_report][:hostname]}\n::1 ip6-localhost ip6-loopback #{node[:alm_report][:hostname]}\nfe00::0 ip6-localnet\nff00::0 ip6-mcastprefix\nff02::1 ip6-allnodes\nff02::2 ip6-allrouters\nff02::3 ip6-allhosts\n"
+end
+
+service 'hostname' do
+  action :restart
+end
+
 # Create shared folders and set permissions
-%w{ alm-report alm-report/current alm-report/current/public alm-report/shared alm-report/shared/config alm-report/releases }.each do |dir|
+%w{ alm alm/current alm/shared alm/releases }.each do |dir|
   directory "/var/www/#{dir}" do
-    owner node[:alm][:user]
-    group node[:alm][:group]
+    owner node[:alm_report][:user]
+    group node[:alm_report][:group]
     mode 0755
     recursive true
   end
@@ -21,38 +48,38 @@ end
 require 'securerandom'
 # Create new settings.yml unless it exists already
 # Set these passwords in config.json to keep them persistent
-unless File.exists?("/var/www/alm-report/shared/config/settings.yml")
+unless File.exists?("/var/www/alm/shared/config/settings.yml")
   node.set['alm']['key'] = SecureRandom.hex(30) unless node['alm']['key']
   node.set['alm']['secret'] = SecureRandom.hex(30) unless node['alm']['secret']
   node.set['alm']['api_key'] = SecureRandom.hex(30) unless node['alm']['api_key']
 else
-  settings = YAML::load(IO.read("/var/www/alm-report/shared/config/settings.yml"))
-  rest_auth_site_key = settings["#{node[:alm][:environment]}"]["rest_auth_site_key"]
-  secret_token = settings["#{node[:alm][:environment]}"]["secret_token"]
-  api_key = settings["#{node[:alm][:environment]}"]["api_key"]
+  settings = YAML::load(IO.read("/var/www/alm/shared/config/settings.yml"))
+  rest_auth_site_key = settings["#{node[:alm_report][:environment]}"]["rest_auth_site_key"]
+  secret_token = settings["#{node[:alm_report][:environment]}"]["secret_token"]
+  api_key = settings["#{node[:alm_report][:environment]}"]["api_key"]
 
   node.set_unless['alm']['key'] = rest_auth_site_key
   node.set_unless['alm']['secret'] = secret_token
   node.set_unless['alm']['api_key'] = api_key
 end
 
-template "/var/www/alm-report/shared/config/settings.yml" do
+template "/var/www/alm/shared/config/settings.yml" do
   source 'settings.yml.erb'
-  owner node[:alm][:user]
-  group node[:alm][:group]
+  owner node[:alm_report][:user]
+  group node[:alm_report][:group]
   mode 0644
 end
 
 # Create new database.yml unless it exists already
 # Set these passwords in config.json to keep them persistent
-unless File.exists?("/var/www/alm-report/shared/config/database.yml")
+unless File.exists?("/var/www/alm/shared/config/database.yml")
   node.set_unless['mysql']['server_root_password'] = SecureRandom.hex(8)
   node.set_unless['mysql']['server_repl_password'] = SecureRandom.hex(8)
   node.set_unless['mysql']['server_debian_password'] = SecureRandom.hex(8)
   database_exists = false
 else
-  database = YAML::load(IO.read("/var/www/alm-report/shared/config/database.yml"))
-  server_root_password = database["#{node[:alm][:environment]}"]["password"]
+  database = YAML::load(IO.read("/var/www/alm/shared/config/database.yml"))
+  server_root_password = database["#{node[:alm_report][:environment]}"]["password"]
 
   node.set_unless['mysql']['server_root_password'] = server_root_password
   node.set_unless['mysql']['server_repl_password'] = server_root_password
@@ -60,18 +87,26 @@ else
   database_exists = true
 end
 
-template "/var/www/alm-report/shared/config/database.yml" do
+template "/var/www/alm/shared/config/database.yml" do
   source 'database.yml.erb'
-  owner node[:alm][:user]
-  group node[:alm][:group]
+  owner node[:alm_report][:user]
+  group node[:alm_report][:group]
   mode 0644
 end
 
 include_recipe "mysql::server"
 include_recipe "database::mysql"
 
+# Add configuration settings to database seed files
+template "/var/www/alm/shared/db/seeds/_custom_sources.rb" do
+  source '_custom_sources.rb.erb'
+  owner node[:alm_report][:user]
+  group node[:alm_report][:group]
+  mode 0644
+end
+
 # Create default MySQL database
-mysql_database "#{node[:alm][:name]}_#{node[:alm][:environment]}" do
+mysql_database "#{node[:alm_report][:name]}_#{node[:alm_report][:environment]}" do
   connection(
     :host     => 'localhost',
     :username => 'root',
@@ -80,8 +115,6 @@ mysql_database "#{node[:alm][:name]}_#{node[:alm][:environment]}" do
   action :create
 end
 
-node.set_unless['passenger']['root_path'] = "/var/lib/gems/1.9.1/gems/passenger-#{node['passenger']['version']}"
-node.set_unless['passenger']['module_path'] = "/var/lib/gems/1.9.1/gems/passenger-#{node['passenger']['version']}/ext/apache2/mod_passenger.so"
 include_recipe "passenger_apache2::mod_rails"
 
 execute "disable-default-site" do
@@ -89,6 +122,8 @@ execute "disable-default-site" do
 end
 
 web_app "alm-report" do
-  template "alm-report.conf.erb"
-  notifies :restart, resources(:service => "apache2"), :delayed
+  docroot "/var/www/alm-report/current/public"
+  server_name node[:alm_report][:host]
+  server_aliases [ node[:alm_report][:host] ]
+  rails_env node[:alm_report][:environment]
 end
