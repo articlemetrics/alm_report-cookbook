de3c44356028814608231c926c564e94d914428c
Martin Fenner <mfenner@plos.org>
2014-08-14 00:14:24 +0200

    don't restart ohai

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,12 +25,12 @@ end
 
 file '/etc/hostname' do
   content "#{node[:alm_report][:hostname]}\n"
-  notifies :reload, 'ohai[reload]'
+  notifies :reload
 end
 
 file '/etc/hosts' do
   content "127.0.0.1  localhost #{node[:alm_report][:hostname]}\n::1 ip6-localhost ip6-loopback #{node[:alm_report][:hostname]}\nfe00::0 ip6-localnet\nff00::0 ip6-mcastprefix\nff02::1 ip6-allnodes\nff02::2 ip6-allrouters\nff02::3 ip6-allhosts\n"
-  notifies :reload, 'ohai[reload]'
+  notifies :reload
 end
 
 # Create shared folders and set permissions
