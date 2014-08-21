4691a2a2bbc4036707aa2b3359764c7161dd1325
Jure Triglav <juretriglav@gmail.com>
2014-08-21 23:28:54 +0200

    Add automatic hostname determination for AWS/EC2 instances.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,20 @@
-default[:alm_report][:name] = "alm-report"
-default[:alm_report][:host] = "33.33.44.55"
-default[:alm_report][:environment] = "development"
-default[:alm_report][:useragent] = "ALM Reports"
-default[:alm_report][:hostname] = "example.com"
-default[:alm_report][:api_key] = nil
-default[:alm_report][:admin] = { username: "articlemetrics", name: "Admin", email: "admin@example.com", password: nil }
-default[:alm_report][:key] = nil
-default[:alm_report][:secret] = nil
-default[:alm_report][:user] = "vagrant"
-default[:alm_report][:group] = "www-data"
-default[:alm_report][:ip_range_us] = nil
-default[:alm_report][:ip_range_uk] = nil
+default['alm_report']['name'] = 'alm-report'
+default['alm_report']['host'] = '33.33.44.55'
+default['alm_report']['environment'] = 'development'
+default['alm_report']['useragent'] = 'ALM Reports'
+default['alm_report']['hostname'] = 'example.com'
+default['alm_report']['api_key'] = nil
+default['alm_report']['admin'] = {
+  'username' => 'articlemetrics',
+  'name' => 'Admin',
+  'email' => 'admin@example.com',
+  'password' => nil
+}
+default['alm_report']['key'] = nil
+default['alm_report']['secret'] = nil
+default['alm_report']['user'] = 'vagrant'
+default['alm_report']['group'] = 'www-data'
+default['alm_report']['ip_range_us'] = nil
+default['alm_report']['ip_range_uk'] = nil
 
-default[:apache][:version] = '2.4'
+default['apache']['version'] = '2.4'
