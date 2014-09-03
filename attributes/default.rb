a313275f4d4946807f440a87bf74975ee21499e3
Martin Fenner <mfenner@plos.org>
2014-09-03 02:20:18 +0200

    allow configuration of API server

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ default['ruby']['packages'] = %w{ libxml2-dev libxslt-dev libmysqlclient-dev nod
 
 # config/settings.yml
 default['alm_report']['settings'] = {
+  "api_server"   => "http://alm.plos.org",
   "api_key"      => SecureRandom.hex(20),
   "username"     => "",
   "password"     => "",
