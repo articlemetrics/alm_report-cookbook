75bf8d47b8b5520db966380100196af4e6b092b4
Martin Fenner <mfenner@plos.org>
2014-12-03 03:26:29 -0800

    install nodejs via cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common nodejs npm }
+default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
 default["dotenv"] = "default"
 default["application"] = "alm-report"
 default['ruby']['api_only'] = true
