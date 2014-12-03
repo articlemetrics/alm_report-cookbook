08c06fb2286ba656e0271da2b1842bd423a75a3d
Martin Fenner <mfenner@plos.org>
2014-12-03 01:57:10 -0800

    correct default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['ruby']['packages'] = %w{ curl git libmysqlclient-dev libpq-dev avahi-daemon libnss-mdns python-software-properties software-properties-common nodejs npm }
+default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common nodejs npm }
 default["dotenv"] = "default"
-default["application"] = "lagotto"
+default["application"] = "alm-report"
 default['ruby']['api_only'] = true
