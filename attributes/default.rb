48b7a7f1c63a2633ff4ab9b9b04f4ed30f934ba2
Martin Fenner <mfenner@plos.org>
2014-12-10 00:33:55 +0100

    fixed syntax error

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
 default["dotenv"] = "default"
 default["application"] = "alm-report"
-node['nodejs']['install_method'] = "source"
+default['nodejs']['install_method'] = "source"
