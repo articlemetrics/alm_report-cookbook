9170e5cb3b5ec63e26c7eb0c6112e1629b612d16
Martin Fenner <mfenner@plos.org>
2014-12-09 23:19:49 +0100

    use latest version of mysql cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configures ALM Reports application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.6.4"
+version           "0.6.5"
 
 # opscode cookbooks
 depends           "apt"
 depends           "memcached"
-depends           "mysql", '~> 5.4.3'
+depends           "mysql"
 depends           "database"
 depends           "nodejs"
 
