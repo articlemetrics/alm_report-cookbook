9e657de9b8ae7695491b0b4646c071df6293494f
Martin Fenner <mfenner@plos.org>
2014-12-10 22:42:17 +0100

    use correct checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@ default["dotenv"] = "default"
 default["application"] = "alm-report"
 default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = "0.10.33"
-default['nodejs']['binary']['checksum']['linux_x64'] = "4eba69caf7368d7f388700eb02996f85b06f457a"
+default['nodejs']['binary']['checksum']['linux_x64'] = "159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8"
 default['nodejs']['npm']['version'] = "1.4.28"
 default['npm_packages'] = %w{ bower }
