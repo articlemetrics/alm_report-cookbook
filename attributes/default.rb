6edc2b3f23e02e00fabf79c86375fb9de260a1b6
Martin Fenner <mfenner@plos.org>
2014-12-10 21:48:13 +0100

    install node and npm via binary

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
 default["dotenv"] = "default"
 default["application"] = "alm-report"
-default['nodejs']['install_method'] = "source"
+default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = "0.10.33"
 default['nodejs']['npm']['version'] = "1.4.28"
 default['npm_packages'] = %w{ bower }
