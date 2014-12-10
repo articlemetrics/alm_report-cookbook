c1deb16af7f3f4b5deb3e0e989bf1b2a1a9b2c26
Martin Fenner <mfenner@plos.org>
2014-12-10 01:18:59 +0100

    install nodejs from source

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
 default["dotenv"] = "default"
 default["application"] = "alm-report"
+default['nodejs']['install_method'] = "source"
