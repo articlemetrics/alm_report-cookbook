5d494747561d6a4f40dcde62e388e5b71c5b81ab
Martin Fenner <mfenner@plos.org>
2014-12-10 22:38:46 +0100

    use correct node checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,8 @@
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
 default["dotenv"] = "default"
 default["application"] = "alm-report"
-default['nodejs']['install_method'] = "source"
+default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = "0.10.33"
-# default['nodejs']['npm']['version'] = "1.4.28"
+default['nodejs']['binary']['checksum']['linux_x64'] = "4eba69caf7368d7f388700eb02996f85b06f457a"
+default['nodejs']['npm']['version'] = "1.4.28"
 default['npm_packages'] = %w{ bower }
