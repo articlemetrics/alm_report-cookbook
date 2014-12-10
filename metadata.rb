84db9232245ee4f991daaeb887e50b447ff576e5
Martin Fenner <mfenner@plos.org>
2014-12-10 22:02:53 +0100

    install previous node version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configures ALM Reports application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.6.17"
+version           "0.6.18"
 
 # opscode cookbooks
 depends           "apt"
