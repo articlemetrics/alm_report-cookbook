e9f8c92a0e851f767ac8d300a3bf5423f0ca92b0
Martin Fenner <mfenner@plos.org>
2014-12-10 11:27:04 +0100

    install bower

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libns
 default["dotenv"] = "default"
 default["application"] = "alm-report"
 default['nodejs']['install_method'] = "source"
+default['nodejs']['npm_packages'] = [{ "name" => "bower" }]
