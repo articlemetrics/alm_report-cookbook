ceae57a2c957f473334abeb29eb361579b292a22
Martin Fenner <mfenner@plos.org>
2015-01-26 12:27:10 +0100

    install nodejs binary

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
-default['ruby']['packages'] = %w{ curl git libmysqlclient-dev nodejs avahi-daemon libnss-mdns python-software-properties software-properties-common }
+default['ruby']['packages'] = %w{ curl git libmysqlclient-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
 default["dotenv"] = "default"
 default["application"] = "alm-report"
-# default['nodejs']['install_method'] = "binary"
-# default['nodejs']['version'] = "0.10.33"
-# default['nodejs']['binary']['checksum']['linux_x64'] = "159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8"
-# default['nodejs']['npm']['version'] = "1.4.28"
+default['nodejs']['install_method'] = "binary"
+default['nodejs']['version'] = "0.10.33"
+default['nodejs']['binary']['checksum']['linux_x64'] = "159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8"
+default['nodejs']['npm']['version'] = "1.4.28"
 default['npm_packages'] = %w{ bower }
